c35b66a5ed0b4aa609f9f49dbbca45be66766395
HustleGengTech <154530201+HustleGengTech@users.noreply.github.com>
2025-03-28 01:31:38 +0100

    Update settings.py

--- errand/settings.py
+++ errand/settings.py
@@ -28,10 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-if ENVIRONMENT == 'production':
-    DEBUG = True
-else:
-    DEBUG = False
+DEBUG = False
 
 ALLOWED_HOSTS = ['localhost','127.0.0.1','errand-app.up.railway.app']
 
